06bc6e4e18319f381b7d0d8a66d6e7e4a0bb838d
agung <agungbagas007@gmail.com>
2020-09-23 05:24:49 +0700

    menambhakan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque id, voluptatibus amet vero sunt, porro odio et ad aperiam esse maiores error repudiandae culpa corrupti mollitia aut. Debitis, delectus praesentium.</p>
+    <ul>
+    <li>list</li>
+    </ul>
 </body>
 </html>
